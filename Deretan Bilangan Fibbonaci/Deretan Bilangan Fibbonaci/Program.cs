addee5b4c4abdb82cf1e9f63fa8f1f328643f7b5
Bela Fadilah <belafdlh@gmail.com>
2021-05-06 17:52:35 +0700

    UTS PBO

---
+++ Deretan Bilangan Fibbonaci/Deretan Bilangan Fibbonaci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Deretan_Bilangan_Fibbonaci
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, agk, k = 1, x = 0;
+            Console.Write("Masukan batas deret :");
+            agk = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i < agk; i++)
+            {
+                if (x >= 0 && x <= agk)
+                {
+                    Console.Write("{0}\t", x);
+                }
+                x = x + k;
+                k = x - k;
+            }
+            Console.ReadLine();
+        }
+    }
+}
+   
